51a5825bd19adcb301a368831f13fb178266fda7
jiuno <bruno.alliani@gmail.com>
2020-06-09 17:26:24 -0300

    MakeCachematrix object made

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	cache <- NULL
+	set<- function(y){
+		cache <<- NULL
+		x <<- y
+	}
+	get <- function() {
+		x
+	}
+	setmean <- function(inverse) {
+		cache <<- inverse
+	}
+	getmean <- function(){
+		cache
+	}
+	list(set = set, 
+	     get = get,
+	     setmean = setmean,
+	     getmean = getmean
+	     )
 }
-
+a<-makeCacheMatrix(x = matrix(1:12,nrow = 4,ncol = 3))
+a$get()
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      
+
 }
